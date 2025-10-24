aab664c5a7f25bcba09d1d56adfbd673b99bfe63
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-10-24 22:28:28 +0700

    Update Next.js configuration to set output format and image optimization options

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // Disable trailing slashes for Capacitor
+  trailingSlash: true,
 };
 
 export default nextConfig;
